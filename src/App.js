9dbcf9fb569e21c50dc88f75edf0b038c15f0c75
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-05-05 11:59:51 -0600

    organize routing/paths using React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import { Home, HowInfokidzWorks, Programs, AboutUs, Contact, Register, PracticeWorksheets, Testimonials, FAQ} from "./components/pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <></>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/how-infokidz-works" element={<HowInfokidzWorks />} />
+          <Route path="/programs" element={<Programs />} />
+          <Route path="/practice-worksheets" element={<PracticeWorksheets />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/testimonials" element={<Testimonials />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
