71981902da49846ac5cb2ecf98df9f7244022fb5
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-05-06 11:59:44 -0600

    style navigation bar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,52 +1,54 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import navbar from "../styling/navbar.css";
 
 function NavBar() {
-    /*Menu Toggle*/
-    const [toggle, setToggle] = useState(flase);
-    const handleToggle = () => setToggle(!toggle);
-    const autoClose = () => setToggle(false);
+  /*Menu Toggle*/
+  const [toggle, setToggle] = useState(false);
+  const handleToggle = () => setToggle(!toggle);
+  const autoClose = () => setToggle(false);
 
-    return ( 
-        <nav id="navbar">
-            {/*Toggle*/}
-            <div className="navbar-toggle" onClick={handleToggle}>
-                <i className={toggle ? "fas fa-times" : "fas fa-bars"} />
-            </div>
+  return (
+    <nav id="navbar">
+      {/*Toggle*/}
+      <div id="navbar-toggle" onClick={handleToggle}>
+        <i className={toggle ? "fas fa-times" : "fas fa-bars"} />
+      </div>
 
-            {/*Items*/}
-            <ul id={toggle ? "navbar-items active" : "navbar-items"}>
-                <li className="navbar-item" id="logo">
-                    <Link to="/" onClick={autoClose}>
-                        
-                    </Link>
-                </li>
-                <li className="navbar-item" id="how-infokidz-works">
-                    <Link to="how-infokidz-works" onClick={autoClose}>
-                        How Infokidz Works
-                    </Link>
-                </li>
-                <li className="navbar-item" id="programs">
-                    <Link to="programs" onClick={autoClose}>
-                        Programs
-                    </Link>
-                </li>
-                <li className="navbar-item" id="about">
-                    About
-                </li>
-                <li className="navbar-item" id="contact">
-                    <Link onClick={autoClose}>
-                        Contact
-                    </Link>
-                </li>
-                <li className="navbar-item" id="register">
-                    <Link onClick={autoClose}>
-                        Register
-                    </Link>
-                </li>
-            </ul>
-        </nav> 
-    );
+      <Link to="/" onClick={autoClose} id="logo">
+        <img
+          id="logo-photo"
+          src={require("../images/infokidz-academy-logo.jpg")}
+          alt="Infokidz Academy Logo"
+        />
+      </Link>
+
+      {/*Items*/}
+      <ul id={toggle ? "navbar-items active" : "navbar-items"}>
+        <li className="navbar-item" id="how-infokidz-works">
+          <Link to="how-infokidz-works" onClick={autoClose} className="link">
+            How Infokidz Works
+          </Link>
+        </li>
+        <li className="navbar-item" id="programs">
+          Programs
+        </li>
+        <li className="navbar-item" id="about">
+          About
+        </li>
+        <li className="navbar-item" id="contact">
+          <Link to="contact" onClick={autoClose} className="link">
+            Contact
+          </Link>
+        </li>
+        <li className="navbar-item" id="register">
+          <Link to="register" onClick={autoClose} className="link">
+            Register
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
