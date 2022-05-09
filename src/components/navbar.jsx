aa5e3ecbf5a5b0a9819e730d01673f01d01d546a
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-05-09 11:39:17 -0600

    integrate and style dropdown menus using React Hooks

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,33 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import navbar from "../styling/navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
 function NavBar() {
-  /*Menu Toggle*/
-  const [toggle, setToggle] = useState(false);
-  const handleToggle = () => setToggle(!toggle);
-  const autoClose = () => setToggle(false);
+  /*Navigation Bar Toggle*/
+  const [navToggle, setNavToggle] = useState(false);
+  const handleNavToggle = () => setNavToggle(!navToggle);
+  const autoCloseNav = () => setNavToggle(false);
+
+  /*Dropdown Menu Toggle - Programs*/
+  const [dropdownTogglePrograms, setDropdownTogglePrograms] = useState(false);
+  const handleDropdownTogglePrograms = () => setDropdownTogglePrograms(!dropdownTogglePrograms);
+  const autoCloseDropdownPrograms = () => setDropdownTogglePrograms(false);
+
+  /*Dropdown Menu Toggle - About*/
+  const [dropdownToggleAbout, setDropdownToggleAbout] = useState(false);
+  const handleDropdownToggleAbout = () => setDropdownToggleAbout(!dropdownToggleAbout);
+  const autoCloseDropdownAbout = () => setDropdownToggleAbout(false);
 
   return (
     <nav id="navbar">
-      {/*Toggle*/}
-      <div id="navbar-toggle" onClick={handleToggle}>
-        <i className={toggle ? "fas fa-times" : "fas fa-bars"} />
+      {/*Navigation Bar Toggle*/}
+      <div id="navbar-toggle" onClick={handleNavToggle}>
+        <i className={navToggle ? "fas fa-times" : "fas fa-bars"} />
       </div>
 
-      <Link to="/" onClick={autoClose} id="logo">
+      <Link to="/" onClick={autoCloseNav} id="logo">
         <img
           id="logo-photo"
           src={require("../images/infokidz-academy-logo.jpg")}
@@ -24,25 +36,94 @@ function NavBar() {
       </Link>
 
       {/*Items*/}
-      <ul id={toggle ? "navbar-items active" : "navbar-items"}>
+      <ul id={navToggle ? "navbar-items active" : "navbar-items"}>
         <li className="navbar-item" id="how-infokidz-works">
-          <Link to="how-infokidz-works" onClick={autoClose} className="link">
+          <Link to="how-infokidz-works" onClick={autoCloseNav} className="link">
             How Infokidz Works
           </Link>
         </li>
-        <li className="navbar-item" id="programs">
-          Programs
+
+        <li
+          className="navbar-item"
+          id={dropdownTogglePrograms ? "programs" : "programs-inactive"}
+          onMouseEnter={handleDropdownTogglePrograms}
+          onMouseLeave={handleDropdownTogglePrograms}
+        >
+          <div id="programs-title">
+            Programs
+            <FontAwesomeIcon icon={faAngleDown} className="dropdown-icon" />
+          </div>
+          
+          <div id="programs-dropdown">
+            <Link 
+              to="programs" 
+              onClick={autoCloseDropdownPrograms} 
+              className="link"
+            >
+              Programs
+            </Link>
+            <Link
+              to="practice-worksheets"
+              onClick={autoCloseDropdownPrograms}
+              className="link"
+              id="practice-worksheets"
+            >
+              Practice Worksheets
+            </Link>
+          </div>
         </li>
-        <li className="navbar-item" id="about">
-          About
+        <li
+          className="navbar-item"
+          id={dropdownToggleAbout ? "about" : "about-inactive"}
+          onMouseEnter={handleDropdownToggleAbout}
+          onMouseLeave={handleDropdownToggleAbout}
+        >
+          <div className="about-title">
+            About
+            <FontAwesomeIcon icon={faAngleDown} className="dropdown-icon" />
+          </div>
+          
+          <div id="about-dropdown">
+            <Link 
+              to="about-us" 
+              onClick={autoCloseDropdownAbout} 
+              className="link"
+            >
+              About Us
+            </Link>
+            <Link
+              to="testimonials"
+              onClick={autoCloseDropdownAbout}
+              className="link"
+            >
+              Testimonials
+            </Link>
+            <Link 
+              to="faq" 
+              onClick={autoCloseDropdownAbout} 
+              className="link" 
+              id="faq"
+            >
+              FAQ
+            </Link>
+          </div>
         </li>
+
         <li className="navbar-item" id="contact">
-          <Link to="contact" onClick={autoClose} className="link">
+          <Link 
+            to="contact" 
+            onClick={autoCloseNav} 
+            className="link"
+          >
             Contact
           </Link>
         </li>
         <li className="navbar-item" id="register">
-          <Link to="register" onClick={autoClose} className="link">
+          <Link 
+            to="register" 
+            onClick={autoCloseNav} 
+            className="link"
+          >
             Register
           </Link>
         </li>
