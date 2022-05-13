e22c380c1bd87a0969c83c223db3a9462d91d81f
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-05-13 11:55:08 -0600

    create home page programs component

---
+++ src/components/programshome.jsx
@@ -0,0 +1,34 @@
+import "../styling/programshome.css";
+import "../styling/button.css";
+import { Link } from "react-router-dom";
+
+function ProgramsHome() {
+  return (
+    <div id="programshome">
+      <h1 id="programshome-title">Programs</h1>
+      <div id="programshome-wrapper">
+        <div className="programshome-program" id="programshome-math">
+          <h2 className="programshome-program-title">Math</h2>
+          <p className="programshome-program-description">
+            Our tutors are prepared to teach students various levels of
+            mathematics, whether it be simple principles like addition or
+            advanced ones like calculus.
+          </p>
+        </div>
+        <div className="programshome-program" id="programshome-ela">
+          <h2 className="programshome-program-title">English Language Arts</h2>
+          <p className="programshome-program-description">
+            Our curriculum is designed to teach students the fundamental
+            elements of English language arts, including reading comprehension,
+            writing, grammar, and speaking.
+          </p>
+        </div>
+      </div>
+      <Link to="programs" className="programshome-learn-more" id="button">
+        Learn More
+      </Link>
+    </div>
+  );
+}
+
+export default ProgramsHome;
