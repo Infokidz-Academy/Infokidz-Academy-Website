fc256e9138cc57bcb04c84c7df480b1d02e37b2d
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-05-19 10:55:00 -0600

    make footer responsive

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,42 +6,41 @@ import { faFacebook, faTwitter } from "@fortawesome/free-brands-svg-icons";
 function Footer() {
   return (
     <footer id="footer">
-      <div id="footer-wrapper">
-        {/*Logo*/}
-        <div id="logo-div">
-          <Link to="/">
-            <img
-              id="footer-logo"
-              src={require("../images/infokidz-academy-logo-black.jpg")}
-              alt="Infokidz Academy Logo"
-            />
-          </Link>
+      <div id="footer-container">
+        <Link to="/">
+          <img
+            id="footer-logo"
+            src={require("../images/infokidz-academy-logo-black.jpg")}
+            alt="Infokidz Academy Logo"
+          />
+        </Link>
+
+        <div id="footer-wrapper">
+          <div id="phone-div">
+            <a id="phone-number" href="tel:780-966-7700">
+              780-966-7700
+            </a>
+          </div>
+
+          <div id="social-medias">
+            <a href="https://www.facebook.com/infokidzacademy/" id="fb">
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+            <a href="https://twitter.com/InfokidzAcademy">
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+          </div>
+
+          <div id="terms-div">
+            <Link to="policies">Terms of Service</Link>
+          </div>
+          <div id="privacy-div">
+            <Link to="policies">Privacy Policy</Link>
+          </div>
         </div>
 
-        <div id="phone-div">
-          <a id="phone-number" href="tel:780-966-7700">
-            780-966-7700
-          </a>
-        </div>
-
-        <div id="social-medias">
-          <a href="https://www.facebook.com/infokidzacademy/" id="fb">
-            <FontAwesomeIcon icon={faFacebook} />
-          </a>
-          <a href="https://twitter.com/InfokidzAcademy">
-            <FontAwesomeIcon icon={faTwitter} />
-          </a>
-        </div>
-
-        <div id="terms-div">
-          <Link to="policies">Terms of Service</Link>
-        </div>
-        <div id="privacy-div">
-          <Link to="policies">Privacy Policy</Link>
-        </div>
+        <h2 id="copyright">Copyright © 2022 Infokidz Academy Inc.</h2>
       </div>
-
-      <h2 id="copyright">Copyright © 2022 Infokidz Academy Inc.</h2>
     </footer>
   );
 }
