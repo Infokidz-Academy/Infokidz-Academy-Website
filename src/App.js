c269ef00baab4203d6e88a574067d3bbcb3b0109
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-05-20 08:06:45 -0600

    update routing to current routes only

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import './App.css';
-import { Home, HowInfokidzWorks, Programs, AboutUs, Contact, Register, PracticeWorksheets, Testimonials, FAQ} from "./components/pages";
+import "./App.css";
+import Home from "./components/pages/home";
+// , HowInfokidzWorks, Programs, AboutUs, Contact, Register, PracticeWorksheets, Testimonials, FAQ, Policies
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/how-infokidz-works" element={<HowInfokidzWorks />} />
+          {/*<Route path="/how-infokidz-works" element={<HowInfokidzWorks />} />
           <Route path="/programs" element={<Programs />} />
           <Route path="/practice-worksheets" element={<PracticeWorksheets />} />
           <Route path="/about-us" element={<AboutUs />} />
@@ -16,6 +17,7 @@ function App() {
           <Route path="/faq" element={<FAQ />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/policies" element={<Policies />} />*/}
         </Routes>
       </BrowserRouter>
     </>
