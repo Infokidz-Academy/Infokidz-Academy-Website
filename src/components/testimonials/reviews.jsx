cfb3878a7be938742a43cb545cdc8ab971072de8
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-05-26 07:52:20 -0600

    create reviews component

---
+++ src/components/testimonials/reviews.jsx
@@ -0,0 +1,22 @@
+import "../../styling/testimonials/reviews.css";
+
+function Reviews() {
+  return (
+    <div id="reviews">
+      <h1 id="reviews-title">Don't just take our word for it:</h1>
+      <div id="reviews-container">
+        <h2 className="review-description" id="testimonials-one">
+          *filler1*
+        </h2>
+        <h2 className="review-description" id="testimonials-two">
+          *filler2*
+        </h2>
+        <h2 className="review-description" id="testimonials-three">
+          *filler3*
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default Reviews;
