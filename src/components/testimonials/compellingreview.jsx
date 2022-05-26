ac2ae2eaca8a4ed6214abc79a39509e9fb3a6a58
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-05-26 07:51:10 -0600

    create compelling review component

---
+++ src/components/testimonials/compellingreview.jsx
@@ -0,0 +1,11 @@
+import "../../styling/testimonials/compellingreview.css";
+
+function CompellingReview() {
+  return (
+    <div id="compellingreview">
+      <h1 id="compellingreview-text">“*filler*”</h1>
+    </div>
+  );
+}
+
+export default CompellingReview;
