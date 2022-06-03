c43069c11dea8930feb57af0ca72e036a7185067
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-06-03 13:15:33 -0600

    refactor dialog using props

--- src/components/formdialog.jsx
+++ src/components/formdialog.jsx
@@ -7,38 +7,27 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 
-function FormDialog() {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
+function FormDialog(props) {
   return (
-    <div>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Open alert dialog
-      </Button>
+    <div id="formdialog">
       <Dialog
-        open={open}
-        onClose={handleClose}
+        open={props.open}
+        onClose={() => props.closeDialog(false)}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">
-          Thank you for Contacting Infokidz!
-        </DialogTitle>
+        <DialogTitle id="alert-dialog-title">{props.title}</DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-            We'll be in touch with you shortly
+            {props.description}
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} autoFocus id="formdialog-button">
+          <Button
+            onClick={() => props.closeDialog(false)}
+            autoFocus
+            id="formdialog-button"
+          >
             Close
           </Button>
         </DialogActions>
