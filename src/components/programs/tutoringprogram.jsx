64c00f73670eb71863a6aac261515dcddd83e614
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-06-06 13:29:21 -0600

    refactor program to remove mui

--- src/components/programs/tutoringprogram.jsx
+++ src/components/programs/tutoringprogram.jsx
@@ -1,30 +1,11 @@
 import "../../styling/programs/tutoringprogram.css";
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
 
 function TutoringProgram(props) {
   return (
     <div id="tutoringprogram">
-      <Typography
-        gutterBottom
-        variant="h5"
-        component="div"
-        id="tutoringprogram-title"
-      >
-        {props.title}
-      </Typography>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">
-              {props.description}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+      <h1 id="tutoringprogram-title">{props.title}</h1>
+      <p>{props.description}</p>
     </div>
   );
 }
