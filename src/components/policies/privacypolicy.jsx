edc2a9f27940d8206eb97fd109e50003bfb6f84a
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-06-24 13:29:09 -0600

    add media queries

--- src/components/policies/privacypolicy.jsx
+++ src/components/policies/privacypolicy.jsx
@@ -15,7 +15,7 @@ function PrivacyPolicy() {
           By using our Web site, www.infokidz.ca you are accepting the policy
           described in the following text.
         </p>
-        <h2 className="privacypolicty-heading">
+        <h2 className="privacypolicy-heading">
           Information Collection, Use and Access
         </h2>
         <p className="privacypolicy-paragraph">
@@ -35,7 +35,7 @@ function PrivacyPolicy() {
           the accuracy and completeness of your Personal Information, and may
           request that it be amended, if appropriate.
         </p>
-        <h2 className="privacypolicty-heading">
+        <h2 className="privacypolicy-heading">
           Safeguarding Customer Information
         </h2>
         <p className="privacypolicy-paragraph">
@@ -44,13 +44,13 @@ function PrivacyPolicy() {
           reasonable precautions to protect your Personal Information from any
           loss or unauthorized use, access or disclosure.
         </p>
-        <h2 className="privacypolicty-heading">Sharing</h2>
+        <h2 className="privacypolicy-heading">Sharing</h2>
         <p className="privacypolicy-paragraph">
           We will not sell or otherwise provide the information we collect to
           outside third parties for the purpose of direct or indirect mass email
           marketing.
         </p>
-        <h2 className="privacypolicty-heading">Links</h2>
+        <h2 className="privacypolicy-heading">Links</h2>
         <p className="privacypolicy-paragraph">
           This web site may contain links to other sites. Please be aware that
           we are not responsible for the content or privacy practices of such
@@ -58,7 +58,7 @@ function PrivacyPolicy() {
           site and to read the privacy statements of any other site that
           collects personally identifiable information.
         </p>
-        <h2 className="privacypolicty-heading">Consent</h2>
+        <h2 className="privacypolicy-heading">Consent</h2>
         <p className="privacypolicy-paragraph">
           Knowledge and consent are required for the collection, use or
           disclosure of Personal Information except where required or permitted
@@ -72,7 +72,7 @@ function PrivacyPolicy() {
           policy or our practices by calling at +1 (780) 9667700 or filling out
           a contact form.
         </p>
-        <h2 className="privacypolicty-heading">Cookies</h2>
+        <h2 className="privacypolicy-heading">Cookies</h2>
         <p className="privacypolicy-paragraph">
           A cookie is a small computer file or piece of information that may be
           stored in your computer's hard drive when you visit our websites. We
