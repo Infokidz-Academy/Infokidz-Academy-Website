11bbb272d86aacdcbc541aed1b47a624fe68f423
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-06-24 14:08:56 -0600

    add navbar and footer to 404

--- src/components/pages/page-not-found.jsx
+++ src/components/pages/page-not-found.jsx
@@ -1,8 +1,16 @@
+import Footer from "../footer";
+import NavBar from "../navbar";
+import "../../styling/pages/page-not-found.css";
+
 function PageNotFound() {
   return (
-    <div id="pagenotfound">
-      <h1 id="pagenotfound-text">404: Sorry, Page Not Found</h1>
-    </div>
+    <>
+      <NavBar />
+      <div id="pagenotfound">
+        <h1 id="pagenotfound-text">404: Sorry, Page Not Found</h1>
+      </div>
+      <Footer />
+    </>
   );
 }
 
