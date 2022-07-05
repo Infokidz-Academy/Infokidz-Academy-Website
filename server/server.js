bba808b8cf4b9ada82ad7fb7a5384b180c6c4758
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-07-05 15:26:06 -0600

    connect server to db

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const port = process.env.PORT || 8000;
+
+// URI Configuration
+dotenv.config();
 
 // App Init
 const app = express();
 app.use(express.json());
+
+// DB Connection
+mongoose.connect(process.env.DB_URI);
 
 // Port
 app.listen(port, () => {
