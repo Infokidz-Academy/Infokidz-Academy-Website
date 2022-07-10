1531ba2efbf3887538675c5ee05fef3abb1df9a6
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-07-09 18:15:22 -0600

    generate build

---
+++ client/build/static/js/371.926d0b8d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinfokidz_academy_website=self.webpackChunkinfokidz_academy_website||[]).push([[371],{2371:function(e,i,n){n.r(i),n.d(i,{default:function(){return c}});var r=n(184);var c=function(){return(0,r.jsx)("div",{id:"compellingreview",children:(0,r.jsx)("h1",{id:"compellingreview-text",children:"\u201c[her] writing skills have improved considerably\u201d"})})}}}]);
+//# sourceMappingURL=371.926d0b8d.chunk.js.map
---
+++ client/build/static/js/377.aee52ddd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinfokidz_academy_website=self.webpackChunkinfokidz_academy_website||[]).push([[377],{8377:function(i,r,t){t.r(r),t.d(r,{default:function(){return n}});t(2791);var e=t(184);var n=function(i){return(0,e.jsxs)("div",{id:"tutoringprogram",children:[(0,e.jsx)("h1",{id:"tutoringprogram-title",children:i.title}),(0,e.jsx)("p",{id:"tutoringprogram-description",children:i.description})]})}}}]);
+//# sourceMappingURL=377.aee52ddd.chunk.js.map
