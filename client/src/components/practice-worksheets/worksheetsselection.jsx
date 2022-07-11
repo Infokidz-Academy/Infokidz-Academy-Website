f581442ad913d2852997e82f80bd7e15fc624377
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-07-11 07:30:36 -0600

    add selection using radio buttons

--- client/src/components/practice-worksheets/worksheetsselection.jsx
+++ client/src/components/practice-worksheets/worksheetsselection.jsx
@@ -8,6 +8,67 @@ function WorksheetsSelection() {
         Select the subject and your sorting preference to view the corresponding
         Infokidz practice worksheets.
       </h2>
+
+      {/*Selection*/}
+      <Card>
+        <CardContent>
+          <Grid item xs={12} sm={6}>
+            <FormControl className="textField">
+              <FormLabel id="demo-radio-buttons-group-label">
+                Subject:
+              </FormLabel>
+              <RadioGroup
+                name="subject"
+                variant="outlined"
+                aria-labelledby="demo-radio-buttons-group-label"
+                defaultValue="Math"
+                row
+              >
+                <FormControlLabel
+                  value="Math"
+                  control={<Radio />}
+                  label={<Typography sx={{ fontSize: 24 }}>Math</Typography>}
+                  onChange={(e) => setSubject(e.target.value)}
+                />
+                <FormControlLabel
+                  value="English"
+                  control={<Radio />}
+                  label={<Typography sx={{ fontSize: 24 }}>English</Typography>}
+                  onChange={(e) => setSubject(e.target.value)}
+                />
+              </RadioGroup>
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <FormControl className="textField">
+              <FormLabel id="demo-radio-buttons-group-label">
+                Sort By:
+              </FormLabel>
+              <RadioGroup
+                name="tutoringReason"
+                variant="outlined"
+                aria-labelledby="demo-radio-buttons-group-label"
+                defaultValue="Grade"
+                row
+              >
+                <FormControlLabel
+                  value="Grade"
+                  control={<Radio />}
+                  label={<Typography sx={{ fontSize: 24 }}>Grade</Typography>}
+                  onChange={(e) => setSort(e.target.value)}
+                />
+                <FormControlLabel
+                  value="Topic"
+                  control={<Radio />}
+                  label={<Typography sx={{ fontSize: 24 }}>Topic</Typography>}
+                  onChange={(e) => setSort(e.target.value)}
+                />
+              </RadioGroup>
+            </FormControl>
+          </Grid>
+        </CardContent>
+      </Card>
     </div>
   );
 }
