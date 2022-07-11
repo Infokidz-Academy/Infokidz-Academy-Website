d8312f79c6710e244548010ff86e648021d80b79
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-07-11 07:21:43 -0600

    create worksheetsselection component

---
+++ client/src/components/practice-worksheets/worksheetsselection.jsx
@@ -0,0 +1,15 @@
+import "../../styling/practice-worksheets/worksheetsselection.css";
+
+function WorksheetsSelection() {
+  return (
+    <div id="worksheetsselection">
+      <h1 id="worksheetsselection-title">Practice Worksheets</h1>
+      <h2 id="worksheetsselection-description">
+        Select the subject and your sorting preference to view the corresponding
+        Infokidz practice worksheets.
+      </h2>
+    </div>
+  );
+}
+
+export default WorksheetsSelection;
