da7146ce1c9384df60d6141263700c8c72cb0647
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-07-13 12:01:08 -0600

    create worksheets component

---
+++ client/src/components/practice-worksheets/worksheets.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import "../../styling/practice-worksheets/worksheets.css";
+
+function Worksheets() {
+  return (
+    <div id="worksheets">
+      {worksheets.map(function (worksheet) {
+        return (
+          <div id="worksheets-container">
+            <h1>{worksheet.name}</h1>
+            <a
+              href={
+                "http://localhost:8000/worksheets/" + worksheet.name + ".pdf"
+              }
+              style={{ color: "red" }}
+            >
+              {worksheet.name}
+            </a>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Worksheets;
