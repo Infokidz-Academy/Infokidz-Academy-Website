b9360b3df2475d5baf80fea677a6d6198fdf0a3d
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-07-14 14:24:40 -0600

    create JS Map collections

--- client/src/components/practice-worksheets/worksheets.jsx
+++ client/src/components/practice-worksheets/worksheets.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 import Axios from "axios";
 import "../../styling/practice-worksheets/worksheets.css";
+import WorksheetsEntry from "./worksheetsentry";
 
 function Worksheets() {
+  // Worksheets data
   const [worksheets, setWorksheets] = useState([]);
+
+  // Obtain worksheets from server
   useEffect(() => {
     async function getWorksheets() {
       const response = await Axios.get("http://localhost:8000/api/worksheets");
@@ -12,25 +16,34 @@ function Worksheets() {
     getWorksheets();
   }, []);
 
-  return (
-    <div id="worksheets">
-      {worksheets.map(function (worksheet) {
-        return (
-          <div id="worksheets-container">
-            <h1>{worksheet.name}</h1>
-            <a
-              href={
-                "http://localhost:8000/worksheets/" + worksheet.name + ".pdf"
-              }
-              style={{ color: "red" }}
-            >
-              {worksheet.name}
-            </a>
-          </div>
-        );
-      })}
-    </div>
-  );
+  // Map for math worksheets sorted by grades
+  let mathGrades = new Map([
+    ["3", []],
+    ["4", []],
+    ["5", []],
+    ["6", []],
+    ["7", []],
+    ["8", []],
+    ["9", []],
+    ["10", []],
+    ["11", []],
+    ["12", []],
+  ]);
+
+  // Map for english worksheets sorted by grades
+  let englishGrades = new Map(mathGrades);
+
+  // Map for math worksheets sorted by topics
+  let mathTopics = new Map([
+    ["Angles", []],
+    ["Area", []],
+    ["Circumference", []],
+    ["Decimal Arithmetic", []],
+    ["Fraction Arithmetic", []],
+  ]);
+
+  // Map for english worksheets sorted by topics
+  let englishTopics = [];
 }
 
 export default Worksheets;
