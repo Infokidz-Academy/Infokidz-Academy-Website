672ea2759485245c99cf7afbba755ee9fa81e867
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-07-14 14:28:08 -0600

    create worksheet sets to render for collections

--- client/src/components/practice-worksheets/worksheets.jsx
+++ client/src/components/practice-worksheets/worksheets.jsx
@@ -93,6 +93,153 @@ function Worksheets() {
   let mathTopicsFractionArithmetic = mathTopics.get("Fraction Arithmetic");
 
   // Variables representing english documents corresponding to each grade
+
+  // Set of worksheets to render for math grades option
+  let mathGradesWorksheets = (
+    <div id="worksheets">
+      <WorksheetsEntry
+        heading="Grade 3"
+        worksheetSet={mathGradesThree}
+        description="Topic"
+      />
+      <WorksheetsEntry
+        heading="Grade 4"
+        worksheetSet={mathGradesFour}
+        description="Topic"
+      />
+      <WorksheetsEntry
+        heading="Grade 5"
+        worksheetSet={mathGradesFive}
+        description="Topic"
+      />
+      <WorksheetsEntry
+        heading="Grade 6"
+        worksheetSet={mathGradesSix}
+        description="Topic"
+      />
+      <WorksheetsEntry
+        heading="Grade 7"
+        worksheetSet={mathGradesSeven}
+        description="Topic"
+      />
+      <WorksheetsEntry
+        heading="Grade 8"
+        worksheetSet={mathGradesEight}
+        description="Topic"
+      />
+      <WorksheetsEntry
+        heading="Grade 9"
+        worksheetSet={mathGradesNine}
+        description="Topic"
+      />
+      <WorksheetsEntry
+        heading="Grade 10"
+        worksheetSet={mathGradesTen}
+        description="Topic"
+      />
+      <WorksheetsEntry
+        heading="Grade 11"
+        worksheetSet={mathGradesEleven}
+        description="Topic"
+      />
+      <WorksheetsEntry
+        heading="Grade 12"
+        worksheetSet={mathGradesTwelve}
+        description="Topic"
+      />
+    </div>
+  );
+
+  // Set of worksheets to render for english grades option
+  let englishGradesWorksheets = (
+    <div id="worksheets">
+      <WorksheetsEntry
+        heading="Grade 3"
+        worksheetSet={englishGradesThree}
+        description="Topic"
+      />
+      <WorksheetsEntry
+        heading="Grade 4"
+        worksheetSet={englishGradesFour}
+        description="Topic"
+      />
+      <WorksheetsEntry
+        heading="Grade 5"
+        worksheetSet={englishGradesFive}
+        description="Topic"
+      />
+      <WorksheetsEntry
+        heading="Grade 6"
+        worksheetSet={englishGradesSix}
+        description="Topic"
+      />
+      <WorksheetsEntry
+        heading="Grade 7"
+        worksheetSet={englishGradesSeven}
+        description="Topic"
+      />
+      <WorksheetsEntry
+        heading="Grade 8"
+        worksheetSet={englishGradesEight}
+        description="Topic"
+      />
+      <WorksheetsEntry
+        heading="Grade 9"
+        worksheetSet={englishGradesNine}
+        description="Topic"
+      />
+      <WorksheetsEntry
+        heading="Grade 10"
+        worksheetSet={englishGradesTen}
+        description="Topic"
+      />
+      <WorksheetsEntry
+        heading="Grade 11"
+        worksheetSet={englishGradesEleven}
+        description="Topic"
+      />
+      <WorksheetsEntry
+        heading="Grade 12"
+        worksheetSet={englishGradesTwelve}
+        description="Topic"
+      />
+    </div>
+  );
+
+  // Set of worksheets to render for math topics option
+  let mathTopicsWorksheets = (
+    <div id="worksheets">
+      <WorksheetsEntry
+        heading="Angles"
+        worksheetSet={mathTopicsAngles}
+        description="Grade"
+      />
+      <WorksheetsEntry
+        heading="Area"
+        worksheetSet={mathTopicsArea}
+        description="Grade"
+      />
+      <WorksheetsEntry
+        heading="Circumference"
+        worksheetSet={mathTopicsCircumference}
+        description="Grade"
+      />
+      <WorksheetsEntry
+        heading="Decimal Arithmetic"
+        worksheetSet={mathTopicsDecimalArithmetic}
+        description="Grade"
+      />
+      <WorksheetsEntry
+        heading="Fraction Arithmetic"
+        worksheetSet={mathTopicsFractionArithmetic}
+        description="Grade"
+      />
+    </div>
+  );
+
+  // Set of worksheets to render for english topics option
+
+  return mathTopicsWorksheets;
 }
 
 export default Worksheets;
