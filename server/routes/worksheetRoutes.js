a7ea1965ab1d26008df4804b5f58fbc3a3535e4e
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-07-27 10:15:23 -0600

    use multer to parse non-file multipart form data

--- server/routes/worksheetRoutes.js
+++ server/routes/worksheetRoutes.js
@@ -7,6 +7,7 @@ const {
 } = require("../controllers/worksheetsController");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const multer = require("multer");
 
 // Router
 const router = express.Router();
@@ -16,6 +17,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+var getFields = multer();
 
 // GET request to obtain all worksheets
 router.get("/worksheets", getWorksheets);
@@ -24,7 +26,7 @@ router.get("/worksheets", getWorksheets);
 router.delete("/delete-worksheet/:id", deleteWorksheet);
 
 // PUT request to update a worksheet
-router.put("/update-worksheet/:id", updateWorksheet);
+router.put("/update-worksheet/:id", getFields.any(), updateWorksheet);
 
 // POST request to create a worksheet
 router.post("/create-worksheet", createWorksheet);
