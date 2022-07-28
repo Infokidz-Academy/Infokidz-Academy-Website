bf98e96bd525056a3794a4e5556f7a6008ef2967
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-07-28 13:03:10 -0600

    refactor worksheetsentries to utilize entry

--- client/src/components/practice-worksheets/worksheetsentries.jsx
+++ client/src/components/practice-worksheets/worksheetsentries.jsx
@@ -1,53 +1,13 @@
 import "../../styling/faq/questions.css";
 import "../../styling/practice-worksheets/worksheetsentries.css";
-import Axios from "axios";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { Button } from "@mui/material";
-import React, { useState } from "react";
+import WorksheetsEntry from "./worksheetsentry";
 
 function WorksheetsEntries(props) {
-  // Edit data
-  const [draftTopic, setDraftTopic] = useState("");
-  const [draftGrade, setDraftGrade] = useState("");
-  const [isEditing, setIsEditing] = useState(false);
-
-  // Delete worksheet HTTP request
-  const handleDelete = (id) => {
-    Axios.delete(`http://localhost:8000/api/delete-worksheet/${id}`);
-  };
-
-  // Update worksheet HTTP request
-  const handleEdit = (id, e) => {
-    e.preventDefault();
-
-    const formData = new FormData();
-
-    // Add data to formData
-    formData.append("draftTopic", draftTopic);
-    formData.append("draftGrade", draftGrade);
-
-    // Refresh input fields
-    setDraftGrade("");
-    setDraftTopic("");
-
-    // Send data to server
-    Axios.put(`http://localhost:8000/api/update-worksheet/${id}`, formData);
-
-    // Remove edit form
-    setIsEditing(false);
-  };
-
-  // Cancel edit form
-  const cancelEdit = () => {
-    setIsEditing(false);
-    setDraftGrade("");
-    setDraftTopic("");
-  };
-
   return (
     <Accordion className="accordion">
       <AccordionSummary
@@ -61,60 +21,11 @@ function WorksheetsEntries(props) {
         <Typography className="accordion-description">
           {props.worksheetSet.map((worksheet) => {
             return (
-              <div>
-                <a href={worksheet.pdfUrl} id="worksheetsentry-link">
-                  {worksheet.name}
-                </a>{" "}
-                | {props.description}:{" "}
-                {props.description === "Topic"
-                  ? worksheet.topic
-                  : worksheet.grade}
-                {props.isAdmin && (
-                  <Button
-                    onClick={() => handleDelete(worksheet._id)}
-                    id="worksheetsentry-delete"
-                    className="button"
-                    variant="contained"
-                  >
-                    Delete
-                  </Button>
-                )}
-                {props.isAdmin && (
-                  <Button
-                    onClick={() => setIsEditing(true)}
-                    id="worksheetsentry-edit"
-                    className="button"
-                    variant="contained"
-                  >
-                    Edit
-                  </Button>
-                )}
-                {isEditing && (
-                  <div id="worksheetsentry-form">
-                    <form
-                      onSubmit={(e) => handleEdit(worksheet._id, e)}
-                      method="post"
-                    >
-                      <input
-                        onChange={(e) => setDraftTopic(e.target.value)}
-                        value={draftTopic}
-                        type="text"
-                        placeholder="Topic"
-                      />
-                      <input
-                        onChange={(e) => setDraftGrade(e.target.value)}
-                        value={draftGrade}
-                        type="text"
-                        placeholder="Grade"
-                      />
-                      <button type="submit">Submit</button>
-                      <button type="button" onClick={() => cancelEdit()}>
-                        Cancel
-                      </button>
-                    </form>
-                  </div>
-                )}
-              </div>
+              <WorksheetsEntry
+                worksheet={worksheet}
+                description={props.description}
+                isAdmin={props.isAdmin}
+              />
             );
           })}
         </Typography>
