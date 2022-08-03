03956d6357fa592328d322a24f3a17fc629d532b
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-08-03 15:56:54 -0600

    develop function for google callback handling

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,6 +3,15 @@ const passport = require("passport");
 // Send google authentication
 const getAuthentication = passport.authenticate("google", ["email"]);
 
+// Google callback after authentication
+const getCallback = passport.authenticate("google", {
+  // URL to bring to user to upon success
+  successRedirect: "/auth/protected",
+  // URL to bring to user to upon failure
+  failureRedirect: "/auth/failure",
+});
+
 module.exports = {
   getAuthentication,
+  getCallback,
 };
