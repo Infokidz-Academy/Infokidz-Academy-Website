90b691e1053054a3c5b8c929738afdf0ae8a951e
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-08-06 09:23:24 -0600

    update client-side references to match new url

--- server/routes/authenticationRoutes.js
+++ server/routes/authenticationRoutes.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: proccess.env.SERVER_URL,
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: proccess.env.SERVER_URL,
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
